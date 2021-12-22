7f928e324d61093bab42b1cc2ad9a74d5c4bb478
hyewon Song <shyewon1203@gmail.com>
2021-12-22 16:08:30 +0900

    8393q 문제풀이

---
+++ algorithm/baekjoon/8393q.java
@@ -0,0 +1,20 @@
+//8393q 
+//1부터 입력받은 N까지의 총 합
+
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int num = scan.nextInt();
+        int result = 0;
+
+        //1부터 입력받은 값까지 반복문으로 더하기
+        for(int i=1;i <= num;i++){
+            result += i;
+        }
+        
+        System.out.println(result);
+    }
+}
