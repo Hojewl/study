9603b47bf27ef30516abddf269606f575903824b
hyewon Song <shyewon1203@gmail.com>
2021-12-23 23:44:08 +0900

    10871q 문제풀이

--- algorithm/baekjoon/Level3/Q10871.java
+++ algorithm/baekjoon/Level3/Q10871.java
@@ -1,3 +1,22 @@
+//10871q 
+// x보다 작은 수만 출력해라
+
+import java.util.*;
+
 public class Q10871 {
-    
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int x = scan.nextInt();
+
+        for(int i=0;i<n;i++){
+            int num = scan.nextInt();
+            if(num < x){
+                System.out.print(num+" ");
+            }
+        }
+
+    }
+
 }
